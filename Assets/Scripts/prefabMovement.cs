a0cf3e0211ed934a6e2145e59c6fb16a3b69182f
LAPTOPAV\agnes <agnesaverenceanu@gmail.com>
2026-03-14 22:29:57 -0400

    Working and bouncing purly

--- Assets/Scripts/prefabMovement.cs
+++ Assets/Scripts/prefabMovement.cs
@@ -1,63 +1,82 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class prefabMovement : MonoBehaviour
+public class PurlyMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float rotationSpeed = 300.0f;
+    Camera cam; // Reference to the main camera
 
-    //2 values to control the speed and rotation of the football
-    [SerializeField] 
-    private float speed = 2.0f; //speed of the snowman
-    [SerializeField] 
-    private float rotationSpeed = 10.0f; //rotation speed of the snowman
+    //initialize the prefab rigidbody property 
+    Rigidbody2D purly;
+    GameObject wall; 
+
     void Start()
     {
-        //nothing to put here 
+        cam = Camera.main; // Get the main camera
+        purly = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component
+        wall = GameObject.Find("wall"); // Find the wall GameObject in the scene
     }
-
-    // Update is called once per frame
 
     void Update()
     {
-        // ----- ROTATION -----
+        // Movement (WASD + Arrow Keys)
+        /*Vector2 move = Vector2.zero;
 
-        // Turn left
-        if (Input.GetKey(KeyCode.R))
-        {
-            transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
-        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            move += Vector2.up;
 
-        // Turn right
-        if (Input.GetKey(KeyCode.F))
-        {
-            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
-        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            move += Vector2.down;
 
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            move += Vector2.right;
 
-        // ----- MOVEMENT -----
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            move += Vector2.left;
 
-        // Move up
+        purly.linearVelocity = move.normalized * speed;*/
+
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+            purly.MovePosition(transform.localPosition += new Vector3(0f, speed * Time.deltaTime, 0f));
         }
 
-        // Move down
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+            purly.MovePosition(transform.localPosition += new Vector3(0f, -speed * Time.deltaTime, 0f));
         }
 
-        // Move right
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            purly.MovePosition(transform.localPosition += new Vector3(speed * Time.deltaTime, 0f, 0f));
         }
 
-        // Move left
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+            purly.MovePosition(transform.localPosition += new Vector3(-speed * Time.deltaTime, 0f, 0f));
         }
+
+        // Rotation (Q/E)
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Find("bottom").Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Find("bottom").Rotate(0f, -rotationSpeed * Time.deltaTime, 0f);
+        }
+
+
     }
 
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject == wall)
+            {
+                purly.linearVelocity = -purly.linearVelocity;
+            }
+        }
 }
